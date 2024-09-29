fe4bd35311042bdaafff7f8307c6d710840248e3
Jrkesari <jrkesari@gmail.com>
2024-09-29 12:17:12 +0530

    Update productApi.js

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const addProduct = (productData) => {
-    return axios.post('/api/products/add', productData);
+    return axios.post('https://mern-1-mvro.onrender.com/api/products/add', productData);
 };
 
 export const updatePrice = (productId, newPrice) => {
-    return axios.put(`/api/products/${productId}/price`, { price: newPrice });
+    return axios.put(`https://mern-1-mvro.onrender.com/api/products/${productId}/price`, { price: newPrice });
 };
 
 export const getSalesReport = () => {
-    return axios.get('/api/products/sales-report');
+    return axios.get('https://mern-1-mvro.onrender.com/api/products/sales-report');
 };
 
 export const addCategory = (categoryData) => {
-    return axios.post('/api/categories/add', categoryData);
+    return axios.post('https://mern-1-mvro.onrender.com/api/categories/add', categoryData);
 };
 
 export const getCategories = () => {
-    return axios.get('/api/categories');
+    return axios.get('https://mern-1-mvro.onrender.com/api/categories');
 };
